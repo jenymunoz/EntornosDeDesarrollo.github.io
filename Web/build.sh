68c5a32b22055d60b916d338fac276852ba42d39
root <jeny.munoz@educa.madrid.org>
2024-10-19 15:50:43 +0200

    Modificando build.sh para que ejecute los scripts.sh

--- Web/build.sh
+++ Web/build.sh
@@ -1,25 +1,42 @@
 #!/usr/bin/bash
 
-echo "vamos a crear cuatro paginas webs"
+echo "Vamos a ejecutar los scripts respectivos para que creen las paginas webs"
 
-echo "Creando index.sh"
-touch index.sh
-echo "Creando pagina1.sh"
-touch pagina1.sh
-echo "creando pagina2.sh"
-touch pagina2.sh
-echo "creando pagina3.sh"
-touch pagina3.sh
+echo "Ejecutando pagina1.sh"
+./pagina1.sh
+echo "Ejecutando pagina2.sh"
+./pagina2.sh
+echo "Ejecutando pagina3.sh"
+./pagina3.sh
 
-echo "vamos a darles permisos de ejecucion"
 
-echo "Dando permisos de ejecucion index.sh"
-chmod +x index.sh
-echo "Dando permisos de ejecucion pagina1.sh"
-chmod +x pagina1.sh
-echo "Dando permisos de ejecucion pagina2.sh"
-chmod +x pagina2.sh
-echo "Dando permisos de ejecucion pagina3.sh"
-chmod +x pagina3.sh
 
-echo "Tarea terminada"
+
+
+
+
+
+
+#echo "vamos a crear cuatro paginas webs"
+
+#echo "Creando index.sh"
+#touch index.sh
+#echo "Creando pagina1.sh"
+#touch pagina1.sh
+#echo "creando pagina2.sh"
+#touch pagina2.sh
+#echo "creando pagina3.sh"
+#touch pagina3.sh
+
+#echo "vamos a darles permisos de ejecucion"
+
+#echo "Dando permisos de ejecucion index.sh"
+#chmod +x index.sh
+#echo "Dando permisos de ejecucion pagina1.sh"
+#chmod +x pagina1.sh
+#echo "Dando permisos de ejecucion pagina2.sh"
+#chmod +x pagina2.sh
+#echo "Dando permisos de ejecucion pagina3.sh"
+#chmod +x pagina3.sh
+
+#echo "Tarea terminada"
